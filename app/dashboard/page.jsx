d64c088b9a58d5cc8eb8074c552cf40d73196383
Leckan <ahmed2real@gmail.com>
2025-02-15 03:28:55 -0800

    created AddCourse page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,12 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
+import AddCourse from './_components/AddCourse'
 
 function page() {
   return (
-    <div></div>
+    <div>
+        <AddCourse/>
+    </div>
   )
 }
 
