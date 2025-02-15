391cbf8bbcf2db4b1d858c938bd7a4f061a57486
Leckan <ahmed2real@gmail.com>
2025-02-15 02:52:06 -0800

    Menu items

---
+++ app/dashboard/_components/SideBar.jsx
@@ -0,0 +1,65 @@
+"use client"
+import Image from 'next/image'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react'
+import { HiHome, HiOutlineShieldCheck, HiOutlineSquare3Stack3D, HiPower } from "react-icons/hi2";
+
+function SideBar() {
+    const Menu = [
+        {
+            id: 1,
+            name: 'Home',
+            icon: <HiHome />,
+            path: '/dashboard'
+        },
+        {
+            id: 2,
+            name: 'Explore',
+            icon: <HiOutlineSquare3Stack3D />,
+            path: '/dashboard/explore'
+        },
+        {
+            id: 3,
+            name: 'Upgrade',
+            icon: <HiOutlineShieldCheck />,
+            path: '/dashboard/upgrade'
+        },
+        {
+            id: 4,
+            name: 'Logout',
+            icon: <HiPower />,
+            path: '/dashboard/logout'
+        }
+
+
+    ]
+
+    const path= usePathname(); 
+
+
+    return (
+        <div className='fixed h-full md:w-64 p-5 shadow-md'>
+            <div className='flex font-bold text-xl items-center' >
+                <Image src={'/coursemakerlogo.svg'} alt='logo' width={50} height={50} />
+                <h2 className='ml-2'>Course Maker AI</h2>
+            </div>
+
+            <hr className='my-5' />
+            <ul>
+                {Menu.map((item, index) => (
+                    <Link href={item.path}><div className={`flex items-center gap-2 text-gray-600
+                    p-3 cursor-pointer hover:bg-gray-100 hover:text-black rounded-lg
+                    ${item.path== path&&'bg-gray-100 text-black'}`}>
+                        <div className='text-2xl'>{item.icon}</div>
+                        <h2>{item.name}</h2>
+                    </div>
+                    </Link>
+
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default SideBar
