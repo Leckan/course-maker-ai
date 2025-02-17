513782726e3c9e82cb188b6c18eaa4a3db462d22
Leckan <ahmed2real@gmail.com>
2025-02-16 22:45:22 -0800

    Add name to dashboard

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider, GoogleOneTap } from "@clerk/nextjs";
 
